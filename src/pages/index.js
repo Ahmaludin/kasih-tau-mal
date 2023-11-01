c953afd750e135c4046f64e46a3edd25a8482eb1
Ahmaludin <ahmal54321@gmail.com>
2023-11-01 11:36:46 +0700

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,6 +32,8 @@ export async function getServerSideProps() {
   return { props: { data } };
 }
 
+// buat fungsi share
+
 // IDEA LISTS
 
 // di halaman article, di tengah konten dibuat box rekomendasi artikle yang berhubungan dengan artikle tsb berdasarkan label, di backend data nya dikirim bareng dengan artikel tsb
